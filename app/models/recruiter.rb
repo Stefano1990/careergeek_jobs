04ffb0c1168e0125ddf8612409065faf662a72ea
Stefano1990 <stefano.hug@gmail.com>
2013-02-15 22:22:14 +0000

    final fix for today.

--- app/models/recruiter.rb
+++ app/models/recruiter.rb
@@ -3,6 +3,7 @@ class Recruiter < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation, :name, :surname, :company, :city, :country
 
   validates       :email, :password, :password_confirmation, :name, :surname, :company, :city, :country, presence: true
+  validates       :email, :password, :password_confirmation, presence: true, on: :create
 
   has_many        :jobs
 
