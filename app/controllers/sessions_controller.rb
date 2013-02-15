2d67cfbe49661393d1c00b0ca3dd820c56bcb7dc
Stefano1990 <stefano.hug@gmail.com>
2013-02-15 22:00:14 +0000

    stupid migrations....

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,14 @@ class SessionsController < ApplicationController
   end
 
   def create
-    employer = Recruiter.authenticate(params[:email], params[:password])
+    recruiter = Recruiter.authenticate(params[:email], params[:password])
     candidate = Candidate.authenticate(params[:email], params[:password])
     if candidate
       session[:candidate_id] = candidate.id
       redirect_to candidate_path(candidate), flash: { success: t("flash.signed_in") }
-    elsif employer
-      session[:employer_id] = employer.id
-      redirect_to employer_path(employer), flash: { success: t("flash.signed_in") }
+    elsif recruiter
+      session[:recruiter_id] = recruiter.id
+      redirect_to recruiter_path(recruiter), flash: { success: t("flash.signed_in") }
     else
       flash.now[:error] = t("flash.invalid_login")
       render "new"
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:candidate_id] = nil
-    session[:employer_id] = nil
+    session[:recruiter_id] = nil
     redirect_to root_path, flash: { success: t("flash.signed_out") }
   end
 end
